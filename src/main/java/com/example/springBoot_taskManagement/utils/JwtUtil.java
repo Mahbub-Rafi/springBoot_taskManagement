464177e2825e45192d26549826cf97e04f9133aa
Mahbub-Rafi <i.m.junior.rafi@gmail.com>
2024-12-26 17:20:34 +0600

    JwtUtil.java functionality implemention. Incomplete as of this day.

---
+++ src/main/java/com/example/springBoot_taskManagement/utils/JwtUtil.java
@@ -0,0 +1,30 @@
+package com.example.springBoot_taskManagement.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtUtil {
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    private String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+                .signWith(getSignningKey(), SignatureAlgorithm.HS256).compact();
+    }
+
+    private Key getSignningKey() {
+//        byte[] keyBytes = new byte[1024];
+//        return null;
+    }
+}
